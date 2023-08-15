3c6448401c59e71fbd44712bd3239270ccf0292f
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-15 11:28:00 -0400

    Add new version to show off what a tuple typing is like and how to pass a reference. oh wait we do not need a reference here

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect: (u32, u32) = (30, 30);
+
+    println!(
+        "
+        The area of the rectangle is {}
+        square pixels.
+        ",
+        area(rect)
+    );
 }
